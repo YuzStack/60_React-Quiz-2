b21fd95df896b62b10b4bd95c988e6713c234c1f
Yusuf Oyinlola <yuzstackinnovation@gmail.com>
2026-03-13 09:48:53 +0100

    Handle displaying progress

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -6,6 +6,7 @@ import Error from './Error';
 import StartScreen from './StartScreen';
 import Question from './Question';
 import NextButton from './NextButton';
+import Progress from './Progress';
 /* eslint-disable */
 
 const initialState = {
@@ -50,6 +51,7 @@ function App() {
   );
 
   const numQuestions = questions.length;
+  const maxPossiblePoints = questions.reduce((acc, cur) => acc + cur.points, 0);
 
   useEffect(function () {
     fetch('http://localhost:9000/questions')
@@ -69,6 +71,13 @@ function App() {
         )}
         {status === 'active' && (
           <>
+            <Progress
+              index={index}
+              numQuestions={numQuestions}
+              points={points}
+              maxPossiblePoints={maxPossiblePoints}
+              answer={answer}
+            />
             <Question
               question={questions.at(index)}
               dispatch={dispatch}
