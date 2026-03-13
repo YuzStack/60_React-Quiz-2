6646809566ca57a7f876b8ee2eb844b4b13f01be
Yusuf Oyinlola <yuzstackinnovation@gmail.com>
2026-03-13 05:18:45 +0100

    Use the useReducer hook to manage the questions and status states

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,9 +1,40 @@
-import DateCounter from './DateCounter';
+import { useEffect, useReducer } from 'react';
+import Header from './Header';
+import Main from './Main';
+/* eslint-disable */
+
+const initialState = {
+  questions: [],
+
+  // This can be either 'loading', 'errror', 'ready', 'active', or 'finished'
+  status: 'loading',
+};
+
+const reducer = function (curState, action) {
+  switch (action.type) {
+    case 'dataReceived':
+      return { ...curState, questions: action.payload, status: 'ready' };
+    case 'dataFailed':
+      return { ...curState, status: 'error' };
+    default:
+      throw new Error('Action unkown');
+  }
+};
 
 function App() {
+  const [state, dispatch] = useReducer(reducer, initialState);
+
+  useEffect(function () {
+    fetch('http://localhost:9000/questions')
+      .then(res => res.json())
+      .then(data => dispatch({ type: 'dataReceived', payload: data }))
+      .catch(_ => dispatch({ type: 'dataFailed' }));
+  }, []);
   return (
-    <div>
-      <DateCounter />
+    <div className='app'>
+      <Header />
+
+      <Main></Main>
     </div>
   );
 }
