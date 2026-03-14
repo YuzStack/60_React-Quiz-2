19bde98e8c8b34ca20b557a8d937757ec110cde9
Yusuf Oyinlola <yuzstackinnovation@gmail.com>
2026-03-14 16:02:29 +0100

    Make the UI responsive to smaller screens

--- src/components/Question.jsx
+++ src/components/Question.jsx
@@ -2,7 +2,7 @@ import Options from './Options';
 
 function Question({ question, dispatch, answer }) {
   return (
-    <div>
+    <div className='question-screen'>
       <h4>{question.question}</h4>
       <Options question={question} dispatch={dispatch} answer={answer} />
     </div>
